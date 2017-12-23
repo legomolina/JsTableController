1c15b16eb1079e4df864928d9f83b81dcbdac756
legomolina <legomolina@gmail.com>
2017-12-24 00:07:05 +0100

    Added insertion of cols

--- js/TableManipulator.js
+++ js/TableManipulator.js
@@ -1,9 +1,8 @@
 var TableManipulator = function () {
-    var table = null;
-    var tableData = null;
-
     var tableManipulator = {
-        init: function (initTable) {
+        selectTable: function (initTable) {
+            var table;
+
             switch (typeof initTable) {
                 case "string":
                     table = document.querySelector(initTable);
@@ -24,40 +23,94 @@ var TableManipulator = function () {
                     return;
             }
 
-            tableData = new TableData(table);
-            console.log(tableData);
+            return new Manipulator(table);
         },
 
-        addRow: function (position) {
-            position = position || -1;
+        createTable: function (rows, cols) {
+            var createdTable = document.createElement("table");
 
-            if (tableData.rows() < position)
+            for (var x = 0; x < rows; x++) {
+                var row = createdTable.insertRow(-1);
+
+                for (var y = 0; y < cols; y++)
+                    row.insertCell(-1);
+            }
+
+            return createdTable;
+        }
+    };
+
+    function Manipulator(t) {
+        var table = t;
+        var tableData = new TableData(table);
+
+        this.addRow = function (position) {
+            if (position === undefined || position > tableData.rows())
                 position = -1;
 
             return table.insertRow(position);
-        },
+        };
 
-        addNRows: function (count, position) {
-            position = position || -1;
-            count = count || 1;
+        this.addNRows = function (count, startPosition) {
+            if (startPosition === undefined || startPosition > tableData.rows())
+                startPosition = -1;
+
+            count = (count === undefined) ? 1 : count;
 
             var rows = [];
 
             for (var i = 0; i < count; i++) {
-                rows.push(this.addRow(position));
+                rows.push(this.addRow(startPosition));
 
-                position = (position === -1) ? -1 : position + 1;
+                startPosition = (startPosition === -1) ? -1 : startPosition + 1;
             }
 
             return rows;
-        },
+        };
 
-        getTableData: function () {
+        this.addColumn = function (position) {
+            if (position === undefined || position > tableData.cells())
+                position = -1;
+
+            var cells = [];
+
+            for (var i = 0; i < tableData.rows(); i++)
+                cells.push(addCell(i, position));
+
+            return cells;
+        };
+
+        this.addNColumns = function (count, startPosition) {
+            if (startPosition === undefined || startPosition > tableData.cells())
+                startPosition = -1;
+
+            count = (count === undefined) ? 1 : count;
+
+            var cells = [];
+
+            for (var i = 0; i < count; i++) {
+                cells.push(this.addColumn(startPosition));
+
+                startPosition = (startPosition === -1) ? -1 : startPosition + 1;
+            }
+
+            return cells;
+        };
+
+        this.getTableData = function () {
             return tableData;
+        };
+
+        function addCell(rowIndex, cellPosition) {
+            if (rowIndex === undefined || rowIndex > tableData.rows())
+                rowIndex = -1;
+
+            if (cellPosition === undefined || cellPosition > tableData.cells())
+                cellPosition = -1;
+
+            return table.rows[rowIndex].insertCell(cellPosition);
         }
-
-    };
-
+    }
 
     return tableManipulator;
 }();
